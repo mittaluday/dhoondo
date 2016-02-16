3a3bbd6e0fa2f056a1fba5f85b4909e292bd4343
Uday Mittal <uday19@gmail.com>
2016-02-16 13:03:38 -0800

    Changes to tokenizing logic

--- data-processor/src/main/java/in/mittaluday/data_processor/FileTokenizer.java
+++ data-processor/src/main/java/in/mittaluday/data_processor/FileTokenizer.java
@@ -1,11 +1,15 @@
 package in.mittaluday.data_processor;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -15,32 +19,35 @@ import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class FileTokenizer {
-	
+
 	private File file;
-	private HashMap<String, Integer>tokenCount;
+	private HashMap<String, Integer> tokenCount;
 	private ArrayList<String> listOfTokens;
 	private ArrayList<String> listofThreeGrams;
 	private HashMap<String, Integer> threeGramsFrequency;
 	private String subdomain;
 	private String subdomainURL;
 
-	public FileTokenizer(){
+	private static final String specialCharacters = ":;,~!@#$%^&*()_+=\\.\"<>?/`\\W";
+	private static final String delimeters = "[ " + specialCharacters + "]+";
+
+	public FileTokenizer() {
 		tokenCount = new HashMap<String, Integer>();
 		listOfTokens = new ArrayList<String>();
 		listofThreeGrams = new ArrayList<String>();
-		threeGramsFrequency = new HashMap<String, Integer>();	
+		threeGramsFrequency = new HashMap<String, Integer>();
 	}
 
 	public FileTokenizer(File file) {
 		this();
 		this.file = file;
 	}
-	
-	public List<String> getListOfTokens(){
+
+	public List<String> getListOfTokens() {
 		return listOfTokens;
 	}
-	
-	public void initialize(File file){
+
+	public void initialize(File file) {
 		tokenCount = new HashMap<String, Integer>();
 		listOfTokens = new ArrayList<String>();
 		listofThreeGrams = new ArrayList<String>();
@@ -73,139 +80,154 @@ public class FileTokenizer {
 	}
 
 	/**
-	 * First line of the dump file contains the URL
-	 * This method extracts the subdomainURL
+	 * First line of the dump file contains the URL This method extracts the
+	 * subdomainURL
+	 * 
 	 * @param fileHandle
 	 */
-	public void parseSubdomain(Scanner fileHandle){
+	public void parseSubdomain(Scanner fileHandle) {
 		this.subdomainURL = fileHandle.nextLine().trim();
 	}
-	
+
 	/**
 	 * Method to parse the subdomain from the URL
+	 * 
 	 * @param url
 	 * @return
 	 */
 	public String parseURLForSubDomain(String url) {
 		int startpos = url.indexOf("/", url.indexOf("/") + 1);
-		int endpos = url.indexOf("/", startpos+1);
-		if(endpos == -1){
+		int endpos = url.indexOf("/", startpos + 1);
+		if (endpos == -1) {
 			endpos = url.length();
 		}
-		return url.substring(startpos+1, endpos);
+		return url.substring(startpos + 1, endpos);
 	}
-	
-	public void tokenizeFile() throws FileNotFoundException{
+
+	public void tokenizeFile() throws FileNotFoundException {
 		Scanner fileReader = new Scanner(file);
-		if(fileReader.hasNextLine()){
+		if (fileReader.hasNextLine()) {
 			parseSubdomain(fileReader);
 			this.subdomain = parseURLForSubDomain(this.subdomainURL);
 		}
-		while(fileReader.hasNextLine()){
+		while (fileReader.hasNextLine()) {
 			String line = fileReader.nextLine();
-			String tokens[] = line.split("[ ,();:\"]");
-			for(int i =0;i<tokens.length;i++){
+			String tokens[] = line.split(delimeters);
+			for (int i = 0; i < tokens.length; i++) {
 				String trimmedToken = tokens[i].trim();
-				if(trimmedToken.length()>0){
+				if (trimmedToken.length() > 0) {
 					listOfTokens.add(trimmedToken.toLowerCase());
 					incrementTokenCount(trimmedToken.toLowerCase());
-				}				
+				}
 			}
 		}
 		fileReader.close();
 	}
-	
-	public void computeTokenFrequencies(List<String> tokens){
-		for(int i=0;i<tokens.size();i++){
+
+	public void computeTokenFrequencies(List<String> tokens) {
+		for (int i = 0; i < tokens.size(); i++) {
 			incrementTokenCount(tokens.get(i).toLowerCase());
 		}
 	}
-	
-	public void computeThreeGrams(List<String> tokens){
-		int threeGramCount=1;
+
+	static HashSet<String> stopworddict;
+
+	public static void loadDictionary() throws IOException, ClassNotFoundException {
+		String stopworddictDictFilePath = "stopwords.ser";
+		File stopwordDictFile = new File(stopworddictDictFilePath);
+		FileInputStream f = new FileInputStream(stopworddictDictFilePath);
+		ObjectInputStream s = new ObjectInputStream(f);
+		stopworddict = (HashSet<String>) s.readObject();
+		s.close();
+		f.close();
+	}
+
+	public void computeThreeGrams(List<String> tokens) throws ClassNotFoundException, IOException {
+		int threeGramCount = 1;
+		loadDictionary();
 		String firstGram = "", secondGram = "", thirdGram = "";
-		for(int i=0;i<tokens.size();i++){
+		for (int i = 0; i < tokens.size(); i++) {
 			String token = tokens.get(i);
-			if(threeGramCount == 1){
-				firstGram = token.toLowerCase();
-				threeGramCount++;
-			} else if (threeGramCount == 2){
-				firstGram = token.toLowerCase();
-				threeGramCount++;
-			} else {
-				thirdGram = token.toLowerCase();
-				String temp = firstGram + " " + secondGram + " " + thirdGram;
-				listofThreeGrams.add(temp);
-				incrementThreeGramCount(temp);
-				firstGram = secondGram; 
-				secondGram = thirdGram;
+			if (!stopworddict.contains(token)) {
+				if (threeGramCount == 1) {
+					firstGram = token.toLowerCase();
+					threeGramCount++;
+				} else if (threeGramCount == 2) {
+					firstGram = token.toLowerCase();
+					threeGramCount++;
+				} else {
+					thirdGram = token.toLowerCase();
+					String temp = firstGram + " " + secondGram + " " + thirdGram;
+					listofThreeGrams.add(temp);
+					incrementThreeGramCount(temp);
+					firstGram = secondGram;
+					secondGram = thirdGram;
+				}
 			}
 		}
 	}
-	
 
 	private void incrementTokenCount(String token) {
-		if(tokenCount.containsKey(token)){
+		if (tokenCount.containsKey(token)) {
 			int count = tokenCount.get(token);
-			tokenCount.put(token, count+1);
+			tokenCount.put(token, count + 1);
 		} else {
 			tokenCount.put(token, 1);
-		}		
+		}
 	}
-	
+
 	private void incrementThreeGramCount(String threeGram) {
-		if(threeGramsFrequency.containsKey(threeGram)){
+		if (threeGramsFrequency.containsKey(threeGram)) {
 			int count = threeGramsFrequency.get(threeGram);
-			threeGramsFrequency.put(threeGram, count+1);
+			threeGramsFrequency.put(threeGram, count + 1);
 		} else {
 			threeGramsFrequency.put(threeGram, 1);
 		}
 	}
 
-	void printTokens(){
+	void printTokens() {
 		System.out.println("***************Printing tokens:");
-		for(int i=0; i<listOfTokens.size();i++){
+		for (int i = 0; i < listOfTokens.size(); i++) {
 			System.out.println(listOfTokens.get(i));
 		}
 	}
-	
-	void printTokenFrequencies(){
-		System.out.println("***************Printing tokens count:");	    
-	    HashMap<String, Integer> sortedMap = sortMap(tokenCount); 
-	    Iterator<Entry<String, Integer>> it = sortedMap.entrySet().iterator();
-	    while (it.hasNext()) {
-	        Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>)it.next();
-	        System.out.println(pair.getKey() + " = " + pair.getValue());
-	    }		
+
+	void printTokenFrequencies() {
+		System.out.println("***************Printing tokens count:");
+		HashMap<String, Integer> sortedMap = sortMap(tokenCount);
+		Iterator<Entry<String, Integer>> it = sortedMap.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>) it.next();
+			System.out.println(pair.getKey() + " = " + pair.getValue());
+		}
 	}
-	
-	void printThreegrams(){
+
+	void printThreegrams() {
 		System.out.println("***************Printing 3-grams count:");
-	    HashMap<String, Integer> sortedMap = sortMap(threeGramsFrequency); 
-	    Iterator<Entry<String, Integer>> it = sortedMap.entrySet().iterator();
-	    while (it.hasNext()) {
-	        Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>)it.next();
-	        System.out.println(pair.getKey() + " = " + pair.getValue());
-	    }
+		HashMap<String, Integer> sortedMap = sortMap(threeGramsFrequency);
+		Iterator<Entry<String, Integer>> it = sortedMap.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>) it.next();
+			System.out.println(pair.getKey() + " = " + pair.getValue());
+		}
 	}
-	
-	public <K, V extends Comparable<? super V>> HashMap<K, V> sortMap( HashMap<K, V> map ){
-	    List<Map.Entry<K, V>> list = new LinkedList<Entry<K, V>>( map.entrySet() );
-	    Collections.sort( list, new Comparator<Map.Entry<K, V>>(){
-	        public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
-	        {
-	            return (o2.getValue()).compareTo( o1.getValue() );
-	        }
-	    } );
-	
-	    HashMap<K, V> result = new LinkedHashMap<K, V>();
-	    for (Map.Entry<K, V> entry : list){
-	        result.put( entry.getKey(), entry.getValue() );
-	    }
-	    return result;
+
+	public <K, V extends Comparable<? super V>> HashMap<K, V> sortMap(HashMap<K, V> map) {
+		List<Map.Entry<K, V>> list = new LinkedList<Entry<K, V>>(map.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+			public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+
+		HashMap<K, V> result = new LinkedHashMap<K, V>();
+		for (Map.Entry<K, V> entry : list) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+		return result;
 	}
-	
-	public ArrayList<String> getListOfThreeGrams(){
+
+	public ArrayList<String> getListOfThreeGrams() {
 		return listofThreeGrams;
 	}
 
--- data-processor/src/main/java/in/mittaluday/data_processor/Starter.java
+++ data-processor/src/main/java/in/mittaluday/data_processor/Starter.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Starter {
 
-	public static void main(String[] args) throws IOException {		
+	public static void main(String[] args) throws IOException, ClassNotFoundException {		
 		CrawledDataProcessor cdp = new CrawledDataProcessor();
 		cdp.startDataProcessing();	
 		System.out.println("Files crawled :" + cdp.getUniquePagesCrawled());
